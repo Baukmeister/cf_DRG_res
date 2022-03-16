718f35c3e017810d7ba2535c5e5ff6a24cc513c9
Florian Bauernfeind <e11775814@student.tuwien.ac.at>
2022-03-16 11:43:32 +0100

    fix logic error in notebook

--- src/visualize.py
+++ src/visualize.py
@@ -10,6 +10,7 @@ def shorten_sequence(sequence):
 
 def plot_sequence(sequence):
     G = nx.DiGraph()
+    items_per_row = 6
 
     short_sequence = shorten_sequence(sequence)
     nodes_with_labels = {i: l for i, l in enumerate(short_sequence)}
@@ -17,7 +18,7 @@ def plot_sequence(sequence):
 
     for node,label in nodes_with_labels.items():
         nodes_with_labels[node] = (
-            re.sub("(.{10})", "\\1-\n", label, 0, re.DOTALL).strip("1-\n")
+            re.sub("(.{11})", "\\1-\n", label, 0, re.DOTALL).strip("1-\n")
         )
 
     G.add_nodes_from(nodes)
@@ -26,9 +27,9 @@ def plot_sequence(sequence):
         if idx != len(short_sequence)-1:
             G.add_edge(idx, idx+1)
 
-    pos = dict(zip(nodes_with_labels.keys(), [[v, 1] for v in range(len(short_sequence))]))
+    pos = dict(zip(nodes_with_labels.keys(), [[(v%items_per_row), 100-(int(v/items_per_row))] for v in range(len(short_sequence))]))
 
-    plt.figure(figsize=(len(short_sequence)*3, 5),dpi=300)
+    plt.figure(figsize=(items_per_row*2.2, int(len(short_sequence)/items_per_row)*3),dpi=300)
     nx.draw(
         G,
         pos=pos,
